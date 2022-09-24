8d94c0d6e657a6660cc4e7ebd1a20a1b07af7e67
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2022-09-24 09:05:31 -0300

    Inicialização do projeto.

--- BankSystem/src/Modulos/Client.java
+++ BankSystem/src/Modulos/Client.java
@@ -1,4 +1,31 @@
 package Modulos;
 
 public class Client {
+
+    String nomeCliente;
+    String dataNascimento;
+    String cpf;
+    String endereco;
+
+    public Client(String nomeCliente, String dataNascimento, String cpf, String endereco) {
+        this.nomeCliente = nomeCliente;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
 }
