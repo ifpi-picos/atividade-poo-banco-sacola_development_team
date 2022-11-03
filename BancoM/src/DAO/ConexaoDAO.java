8ce4b6db1bef2d594b46b08055c7a810b2b37b9c
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2022-11-02 22:07:31 -0300

    Otimização

--- BancoM/src/DAO/ConexaoDAO.java
+++ BancoM/src/DAO/ConexaoDAO.java
@@ -10,7 +10,7 @@ public class ConexaoDAO {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://bancomaut-rds-mysql.c0z9tie9fbsp.sa-east-1.rds.amazonaws.com:3306/bancomaut", "masterAdmin", "Postedeenergia3");
+            conn = DriverManager.getConnection("jdbc:mysql://bancomaut-rds-mysql.c0z9tie9fbsp.sa-east-1.rds.amazonaws.com:3306/bancomaut?useSSL=false", "masterAdmin", "Postedeenergia3");
         } catch (SQLException | ClassNotFoundException e) {
             JOptionPane.showMessageDialog(null, "Erro ao conectar com o banco de dados: " + e.getMessage());
         }
