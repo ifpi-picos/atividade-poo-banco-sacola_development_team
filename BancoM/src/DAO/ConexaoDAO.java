e503a08dbd98479089c32289bca3d7d289fed5ed
CondeArmand <104652384+CondeArmand@users.noreply.github.com>
2022-11-09 16:30:04 -0300

    Correções e otimizações

--- BancoM/src/DAO/ConexaoDAO.java
+++ BancoM/src/DAO/ConexaoDAO.java
@@ -9,9 +9,8 @@ public class ConexaoDAO {
     public Connection conectarBD() {
         Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://bancomaut-rds-mysql.c0z9tie9fbsp.sa-east-1.rds.amazonaws.com:3306/bancomaut?useSSL=false", "masterAdmin", "Postedeenergia3");
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro ao conectar com o banco de dados: " + e.getMessage());
         }
         return conn;
